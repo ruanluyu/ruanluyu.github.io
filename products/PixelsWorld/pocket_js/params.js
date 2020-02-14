e9e2ac6b88e321c6d297995ceb9b8db23ce8b4b9
ruanluyu <zzstarsound@gmail.com>
2020-02-15 05:36:11 +0900

    Change shader effect position

---
+++ products/PixelsWorld/pocket_js/params.js
@@ -0,0 +1,16 @@
+{
+	tabColor: "#a00", editorTheme: "dark", backgroundColor: "#ffffff", width:320,height:180,GUIClosed: true,
+
+	
+
+	textures: [
+		{ 
+			url: "MiLai.png", 
+			wrap: "repeat", // repeat (default), clamp
+			name: "milai",
+		}
+	],
+
+	fragmentShaderFile: "shader.glsl",
+	animated: true,
+}
